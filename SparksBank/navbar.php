bb807eaf7bad94b33f4436f7df6fc43b0543e3ff
PriyankaMMore <90977523+PriyankaMMore@users.noreply.github.com>
2021-12-21 12:48:44 +0530

    Add files via upload

---
+++ SparksBank/navbar.php
@@ -0,0 +1,24 @@
+<!-- navbar --> 
+ <div class="collapsenavbar-collapse" id="collapsibleNavbar">
+     <nav class="navbar navbar-expand-md navbar-light bg-light" >
+      <a class="navbar-brand" href="index.php" style="color : #110000;  "><b>BASIC BANKING SYSTEM</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #110000;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #110000;"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #110000;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #110000;"><b>Transaction History</b></a>
+              </li>
+              
+          </div>
+       </nav> -->
